f76eaedb2303a0e98790c5c309c67bf48e2174e6
Alimov Foziljon <alimovf2018@gmail.com>
2022-04-30 16:33:56 +0500

    init

---
+++ app.js
@@ -0,0 +1,19 @@
+document.getElementById('contact').onclick = openContact
+function openContact(){
+    document.getElementById('modalContact')
+        .style.display = 'flex'
+}
+document.getElementById('close-contact').onclick = ()=>{
+    document.getElementById('modalContact')
+        .style.display = 'none'
+}
+document.getElementById('mobileBtn').onclick = ()=>{
+    document.getElementById('modalMobile')
+        .style.display = 'flex'
+}
+document.getElementById('closeMobile').onclick = closeMobile
+function closeMobile(){
+    document.getElementById('modalMobile')
+        .style.display = 'none'
+    openContact()
+}
